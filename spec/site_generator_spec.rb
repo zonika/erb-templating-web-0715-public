741461318f54913c6de0a28266cff0dd52ff14bd
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 11:16:58 -0500

    Add test files and begin site_generator_spec

--- spec/site_generator_spec.rb
+++ spec/site_generator_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+describe 'SiteGenerator' do
+  let(:site_generator) { SiteGenerator.new }
+
+  describe '#make_index!' do
+    site_generator.make_index!
+    comparison = File.read('spec/fixtures/index.html')
+    index = File.read('_site/index.html')
+    expect(index).to eq(comparison)
+  end
+
+  describe '#generate_pages!' do
+  end
+end
