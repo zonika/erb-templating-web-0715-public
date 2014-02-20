db9ad9bf079e92757eeb8a8abcba557c0711867a
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 11:32:09 -0500

    Update site_generator_spec and add index fixture

--- spec/site_generator_spec.rb
+++ spec/site_generator_spec.rb
@@ -4,6 +4,7 @@ describe 'SiteGenerator' do
   let(:site_generator) { SiteGenerator.new }
 
   describe '#make_index!' do
+    it 'creates an html '
     site_generator.make_index!
     comparison = File.read('spec/fixtures/index.html')
     index = File.read('_site/index.html')
