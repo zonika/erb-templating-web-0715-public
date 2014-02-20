1d8b2f0721eeb19e137b7c5682c231e7fa822e40
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 11:54:05 -0500

    Add test to ensure ERB isn't being used in make_index! method

--- spec/site_generator_spec.rb
+++ spec/site_generator_spec.rb
@@ -4,13 +4,21 @@ describe 'SiteGenerator' do
   let(:site_generator) { SiteGenerator.new }
 
   describe '#make_index!' do
-    it 'creates index.html in the _site directory' do
+    before do
       Movie.reset_movies!
       Movie.make_movies!
+    end
+
+    it 'creates index.html in the _site directory' do
       site_generator.make_index!
       comparison = File.read('spec/fixtures/index.html')
       index = File.read('_site/index.html')
       expect(index).to eq(comparison)
+    end
+
+    it 'does NOT use erb' do
+      expect(ERB).to_not receive(:new)
+      site_generator.make_index!
     end
   end
 
