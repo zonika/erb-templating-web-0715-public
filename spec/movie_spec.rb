8abdd9c6cd622286abc0642806b8100cb6c84d5a
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 10:21:17 -0500

    Fix broken movie_spec setup

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -5,26 +5,23 @@ describe 'Movie' do
   let(:release_date) { 1999 }
   let(:director) { 'The Wachowski Brothers' }
   let(:summary) { 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.' }
-  
-  before(:all) do
-    @movie = Movie.new(title, release_date, director, summary)
-  end
+  let(:movie) { Movie.new(title, release_date, director, summary) }
 
-  context @movie do
+  context 'attributes' do
     it 'has a title' do
-      expect(subject.title).to eq(title)
+      expect(movie.title).to eq(title)
     end
 
     it 'has a release date' do
-      expect(subject.release_date).to eq(release_date)
+      expect(movie.release_date).to eq(release_date)
     end
 
     it 'has a director' do
-      expect(subject.director).to eq(director)
+      expect(movie.director).to eq(director)
     end
 
     it 'has a summary' do
-      expect(subject.summary).to eq(summary)
+      expect(movie.summary).to eq(summary)
     end
   end
 end
