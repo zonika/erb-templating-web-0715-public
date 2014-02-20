c4bceac20f5a88a625cc5a5b99c629e81fc9b36e
Logan Hasson <logan.hasson@gmail.com>
2014-02-20 10:16:25 -0500

    Fix subject in movie_spec

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -5,9 +5,12 @@ describe 'Movie' do
   let(:release_date) { 1999 }
   let(:director) { 'The Wachowski Brothers' }
   let(:summary) { 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.' }
-  let(:movie) { Movie.new(title, release_date, director, summary) }
+  
+  before(:all) do
+    @movie = Movie.new(title, release_date, director, summary)
+  end
 
-  context movie do
+  context @movie do
     it 'has a title' do
       expect(subject.title).to eq(title)
     end
