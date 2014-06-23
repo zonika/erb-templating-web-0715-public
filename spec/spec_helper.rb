e775557c32c0ff0fa4f3c03575f140efa1ab3a47
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-23 12:58:27 -0400

    gemfile lock, rspec 3 config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require_relative '../config/environment'
 Bundler.require(:test)
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'default'
